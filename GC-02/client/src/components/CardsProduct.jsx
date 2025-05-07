8abf6d70ec5458ec67baf2d04a68fb4b9bc16fe8
aXLan666 <angaragilang266@gmail.com>
2025-05-07 20:36:21 +0700

    feat: show info colour snkr and gender

--- GC-02/client/src/components/CardsProduct.jsx
+++ GC-02/client/src/components/CardsProduct.jsx
@@ -16,7 +16,7 @@ function CardsProduct() {
         />
       )}
     >
-      <div className="flex flex-col justify-center gap-2">
+      <div className="flex flex-col justify-center gap-1">
         <div className="group-hover:flex hidden gap-2">
           <img
             className="h-12"
@@ -37,12 +37,14 @@ function CardsProduct() {
         <h5 className="group-hover:hidden text-wrap line-clamp-2 text-base lg:text-lg font-semibold leading-6 text-gray-900 dark:text-dark">
           Nike Air Force 1 '07 EasyOn
         </h5>
-        <p className="group-hover:hidden text-wrap line-clamp-2 text-base font-medium leading-5 text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
+        <p className="group-hover:hidden line-clamp-2 text-base leading-5 text-gray-700 dark:text-gray-500">
+          Men's Shoes
+        </p>
+        <p className="group-hover:hidden line-clamp-2 text-base leading-5 text-gray-700 dark:text-gray-500">
+          3 Colours
         </p>
       </div>
-      <p className="text-xl font-semibold mb-3">Rp 1.549.000</p>
+      <p className="text-xl font-semibold mb-9">Rp 1.549.000</p>
     </Card>
   );
 }

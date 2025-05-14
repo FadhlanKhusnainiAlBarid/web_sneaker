255764ff059a03e85ba4845eaf224e076fdae0bd
aXLan666 <angaragilang266@gmail.com>
2025-05-14 13:26:47 +0700

    feat: show data name, status, price snkr in home

--- IP/client/src/components/CardsProduct.jsx
+++ IP/client/src/components/CardsProduct.jsx
@@ -1,50 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "flowbite-react";
 import { CardTheme } from "../assets/CustomTheme";
+import { Link } from "react-router-dom";
 
-function CardsProduct() {
+let Rupiah = new Intl.NumberFormat("id-ID", {
+  style: "currency",
+  currency: "IDR",
+  minimumFractionDigits: 0,
+});
+
+function CardsProduct({ data }) {
+  const [selectColor, setselectColor] = useState(0);
   return (
     <Card
+      as={Link}
+      to={`/detail/${data.id}`}
       theme={CardTheme}
       className="group cursor-pointer max-w-sm"
       renderImage={() => (
         <img
           width={500}
           height={500}
-          src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e794bcae-1c12-4f44-aeca-76efce96aa04/AIR+FORCE+1+%2707+LV8.png"
+          src={Object.values(data.image)[selectColor][0]}
           alt="image 1"
         />
       )}
     >
       <div className="flex flex-col justify-center gap-1">
-        <div className="group-hover:flex hidden gap-2">
-          <img
-            className="h-12"
-            src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e794bcae-1c12-4f44-aeca-76efce96aa04/AIR+FORCE+1+%2707+LV8.png"
-            alt=""
-          />
-          <img
-            className="h-12"
-            src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e7b6c6c6-cf50-4772-9bfe-0b15ea19461d/AIR+FORCE+1+%2707+LV8.png"
-            alt=""
-          />
-          <img
-            className="h-12"
-            src="https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/ef454901-b884-44b8-a916-1256d599a5ca/AIR+FORCE+1+%2707+LV8.png"
-            alt=""
-          />
+        {Object.keys(data.image).length > 1 && (
+          <>
+            <div className="group-hover:flex hidden gap-2">
+              {Object.entries(data.image).map(([key, value], i) => (
+                <img
+                  key={key}
+                  onMouseEnter={() => setselectColor(i)}
+                  className="h-12"
+                  src={value[0]}
+                  alt=""
+                />
+              ))}
+            </div>
+          </>
+        )}
+        <div
+          className={`${
+            Object.keys(data.image).length > 1 && "*:group-hover:hidden"
+          }`}
+        >
+          <h5 className="text-wrap line-clamp-2 text-base lg:text-lg font-semibold leading-6 text-gray-900 dark:text-dark">
+            {data.name}
+          </h5>
+          <p className="line-clamp-2 text-base leading-5 text-gray-700 dark:text-gray-500">
+            {data.status}
+          </p>
+          <p className="line-clamp-2 text-base leading-5 text-gray-700 dark:text-gray-500">
+            {Object.keys(data.image).length} Colours
+          </p>
         </div>
-        <h5 className="group-hover:hidden text-wrap line-clamp-2 text-base lg:text-lg font-semibold leading-6 text-gray-900 dark:text-dark">
-          Nike Air Force 1 '07 EasyOn
-        </h5>
-        <p className="group-hover:hidden line-clamp-2 text-base leading-5 text-gray-700 dark:text-gray-500">
-          Men's Shoes
-        </p>
-        <p className="group-hover:hidden line-clamp-2 text-base leading-5 text-gray-700 dark:text-gray-500">
-          3 Colours
-        </p>
       </div>
-      <p className="text-xl font-semibold mb-9">Rp 1.549.000</p>
+      <p className="text-xl font-semibold mb-9">{Rupiah.format(data.price)}</p>
     </Card>
   );
 }
--- IP/client/src/pages/HomePage.jsx
+++ IP/client/src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CardsProduct from "../components/CardsProduct";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSnkrs } from "../app/actions";
 
 function Home() {
-  const [data, setdata] = useState([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ]);
+  const dispatch = useDispatch();
+  const { snkrs, loading } = useSelector((state) => state.snkrs);
+
+  useEffect(() => {
+    dispatch(fetchSnkrs());
+  }, []);
+
   return (
     <div className="container mx-auto space-y-6">
       <div className="space-y-3">
@@ -12,9 +18,15 @@ function Home() {
           <h1 className="ml-6 text-lg font-medium">Populer</h1>
         </div>
         <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1.5 lg:gap-2.5 px-0">
-          {data.map((d) => (
-            <CardsProduct key={d} />
-          ))}
+          {loading ? (
+            <h1>...Loading</h1>
+          ) : (
+            <>
+              {snkrs.map((d) => (
+                <CardsProduct key={d.id} data={d} />
+              ))}
+            </>
+          )}
           <div className="mb-12" />
         </div>
       </div>
